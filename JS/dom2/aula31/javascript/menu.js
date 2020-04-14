127d73bb8f9a8101dab4f156fcb007164a9d83c7
jos3s <karatedkgt17@gmail.com>
2020-04-13 21:38:42 -0300

    Adição/Abertura e fechamento do menu

---
+++ JS/dom2/aula31/javascript/menu.js
@@ -0,0 +1,16 @@
+(function () {
+    var $html=document.querySelector('html');
+    var $btn=document.querySelector('.header-nav__hamburgger');
+    var ariaControl=$btn.getAttribute('aria-controls');
+    var $menu=document.getElementById(ariaControl);
+
+    $html.classList.remove("no-js");
+    $html.classList.add('js');
+
+    $btn.addEventListener('click', function () {
+        $html.classList.toggle('menu-opened');
+        var ariaExpanded=this.getAttribute('aria-expanded') === "trye";
+        $btn.setAttribute('aria-expanded', !ariaExpanded);
+        $menu.setAttribute('aria-expanded', !ariaExpanded);
+    })
+})()
