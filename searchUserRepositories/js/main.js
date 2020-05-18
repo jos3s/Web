13bb28f6dd20feebc10b513d7fbcecf875592e50
jos3s <karatedkgt17@gmail.com>
2020-05-18 19:28:45 -0300

    Adição/Animação durente o carregamento dos dados

--- searchUserRepositories/js/main.js
+++ searchUserRepositories/js/main.js
@@ -1,15 +1,26 @@
 const input=document.querySelector('#entrada');
 const form=document.querySelector('form');
+const ul=document.querySelector('.repos');
 
 form.addEventListener('submit',(e)=>{
     e.preventDefault();
     if(input.value!==''){
         axios.get(`https://api.github.com/users/${input.value}/repos`)
+            .then(response=> {
+                ul.innerHTML='';
+                const li=document.createElement('li');
+                const txt=document.createTextNode('Carregando os dados...');
+                li.style.color='rgb(17, 86, 102)';
+                li.appendChild(txt)
+                ul.appendChild(li);
+                return response;
+            })
             .then(dados=> {
-                criarLista(dados.data);
+               setInterval(() => {
+                    criarLista(dados.data);
+               }, 1300);
             })
             .catch(()=>{
-                const ul=document.querySelector('.repos');
                 ul.innerHTML='';
                 const li=document.createElement('li');
                 const txt=document.createTextNode('Erro 404, usuário não encontrado.');
@@ -22,27 +33,34 @@ form.addEventListener('submit',(e)=>{
 });
 
 function criarLista(dados) {
-    const ul=document.querySelector('.repos');
     ul.innerHTML='';
     if(dados.length>0){
         for(let dado of dados){
-            const li=document.createElement('li');
-            const txt=document.createTextNode(dado.name);
-            const p=document.createElement('p');
-            let description = dado.description === null ? document.createTextNode(' '): document.createTextNode(dado.description);
-            p.appendChild(description);
-            const link=document.createElement('a');
-            link.setAttribute("target",'_blank');
-            link.setAttribute('href',dado.html_url);
-            link.appendChild(txt);
-            li.appendChild(link);
-            li.appendChild(p);
-            ul.appendChild(li);
+           criarRepositorio(dado.name,dado.description, dado.html_url)
         }
     }else{
-        const li=document.createElement('li');
-        const txt=document.createTextNode(`Não encontrei nenhum repositório do usuário "${input.value}".`);
-        li.appendChild(txt);
-        ul.appendChild(li);
+        criarMensagemError(`Não encontrei nenhum repositório do usuário "${input.value}".`);
     }
+}
+
+function criarRepositorio(nome, desc,url){
+    const li=document.createElement('li');
+    const txt=document.createTextNode(nome);
+    const p=document.createElement('p');
+    let description = desc === null ? document.createTextNode(' '): document.createTextNode(desc);
+    p.appendChild(description);
+    const link=document.createElement('a');
+    link.setAttribute("target",'_blank');
+    link.setAttribute('href',url);
+    link.appendChild(txt);
+    li.appendChild(link);
+    li.appendChild(p);
+    ul.appendChild(li);
+}
+
+function criarMensagemError(msg) {
+    const li=document.createElement('li');
+    const txt=document.createTextNode(msg);
+    li.appendChild(txt);
+    ul.appendChild(li);
 }
